f70ceef451775f70c57e5d21ca0996f9f9f41571
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-12-01 01:16:45 +0200

    Use bindgen to make Netmap header's bindings.
    These are low level, another library will wrap this one with the Rust
    friendly interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! # Overview
+//!
+//! This library provides low level bindings to netmap - the fast I/O packet framework
+//!
+//! It also exports and environment variable `DEP_NETMAP_INCLUDE` with the include path for the
+//! netmap framework. It can be used by any dependent crate.
+//!
+//! # Notes
+//!
+//! Netmap won't be build, but may be it should in the future.
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/wrapper.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <net/if.h>
+#include <net/netmap.h>
