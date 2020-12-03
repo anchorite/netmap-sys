13e016adffe0324fcefa9745b4e36195c17f6015
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-12-04 01:32:19 +0200

    Build libnetmap.a file from netmap submodule.

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::fs;
 use std::path::PathBuf;
+use std::process;
 
 fn generate_bindings(include_dir: &PathBuf, out_dir: &PathBuf) {
     bindgen::Builder::default()
@@ -13,14 +15,44 @@ fn generate_bindings(include_dir: &PathBuf, out_dir: &PathBuf) {
         .expect("Failed to write bindings");
 }
 
+fn cp_r(from: &PathBuf, to: &PathBuf) {
+    if fs::metadata(from).unwrap().is_file() {
+        let to_file = to.join(from.file_name().unwrap());
+        fs::copy(from, to_file).unwrap();
+    } else {
+        let new_dir_in_to = to.join(from.file_name().unwrap());
+        fs::create_dir(&new_dir_in_to)
+            .unwrap_or_else(|_| println!("Failed to create: {}", new_dir_in_to.to_str().unwrap()));
+        for file_object in from.read_dir().unwrap() {
+            let file_object = file_object.unwrap();
+            cp_r(&file_object.path(), &new_dir_in_to);
+        }
+    }
+}
+
+fn prepare_libnetmap(out_dir: &PathBuf) {
+    cp_r(&PathBuf::from("netmap/libnetmap"), out_dir);
+    process::Command::new("make").current_dir(out_dir.join("libnetmap"));
+}
+
 fn main() {
     // Tell cargo to invalidate crate if changed
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let include_dir = PathBuf::from("netmap/sys");
-    println!("cargo:include={}", include_dir.to_str().unwrap());
+    println!(
+        "cargo:include={}",
+        include_dir
+            .as_path()
+            .canonicalize()
+            .unwrap()
+            .to_str()
+            .unwrap()
+    );
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     generate_bindings(&include_dir, &out_dir);
+
+    prepare_libnetmap(&out_dir);
 }
