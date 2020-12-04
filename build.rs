3bdb70884da93fec7c509152aa2edac3d3108d30
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-12-04 02:09:10 +0200

    Reorganizes and simplifies code.

--- build.rs
+++ build.rs
@@ -3,16 +3,68 @@ use std::fs;
 use std::path::PathBuf;
 use std::process;
 
-fn generate_bindings(include_dir: &PathBuf, out_dir: &PathBuf) {
-    bindgen::Builder::default()
-        .header("src/wrapper.h")
-        .clang_arg(format!("-I{}", include_dir.to_str().unwrap()))
-        //.default_enum_style(bindgen::EnumVariation::NewType { is_bitfield: false })
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Failed to generate bindings")
-        .write_to_file(out_dir.join("bindings.rs"))
-        .expect("Failed to write bindings");
+struct Builder {
+    out_dir: PathBuf,
+    include_dir: PathBuf,
+    src_libnetmap_dir: PathBuf,
+}
+
+impl Builder {
+    pub fn new() -> Self {
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        let include_dir = PathBuf::from("netmap/sys");
+        let libnetmap_dir = PathBuf::from("netmap/libnetmap");
+
+        Self {
+            out_dir,
+            include_dir,
+            src_libnetmap_dir: libnetmap_dir,
+        }
+    }
+
+    pub fn build(&mut self) {
+        self.rerun_if_wrapper_changed();
+        self.export_include_path();
+        self.generate_bindings();
+
+        self.make_libnetmap();
+    }
+
+    fn export_include_path(&self) {
+        println!("cargo:include={}", abs_path(&self.include_dir));
+    }
+
+    fn rerun_if_wrapper_changed(&self) {
+        // Tell cargo to invalidate crate if changed
+        println!("cargo:rerun-if-changed=wrapper.h");
+    }
+
+    fn make_libnetmap(&self) {
+        cp_r(&self.src_libnetmap_dir, &self.out_dir);
+        process::Command::new("make").current_dir(self.out_dir.join("libnetmap"));
+    }
+
+    fn generate_bindings(&self) {
+        bindgen::Builder::default()
+            .header("src/wrapper.h")
+            .clang_arg(format!("-I{}", self.include_dir.to_str().unwrap()))
+            //.default_enum_style(bindgen::EnumVariation::NewType { is_bitfield: false })
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Failed to generate bindings")
+            .write_to_file(self.out_dir.join("bindings.rs"))
+            .expect("Failed to write bindings");
+    }
+}
+
+fn abs_path(pathbuf: &PathBuf) -> String {
+    pathbuf
+        .as_path()
+        .canonicalize()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_owned()
 }
 
 fn cp_r(from: &PathBuf, to: &PathBuf) {
@@ -30,29 +82,6 @@ fn cp_r(from: &PathBuf, to: &PathBuf) {
     }
 }
 
-fn prepare_libnetmap(out_dir: &PathBuf) {
-    cp_r(&PathBuf::from("netmap/libnetmap"), out_dir);
-    process::Command::new("make").current_dir(out_dir.join("libnetmap"));
-}
-
 fn main() {
-    // Tell cargo to invalidate crate if changed
-    println!("cargo:rerun-if-changed=wrapper.h");
-
-    let include_dir = PathBuf::from("netmap/sys");
-    println!(
-        "cargo:include={}",
-        include_dir
-            .as_path()
-            .canonicalize()
-            .unwrap()
-            .to_str()
-            .unwrap()
-    );
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    generate_bindings(&include_dir, &out_dir);
-
-    prepare_libnetmap(&out_dir);
+    Builder::new().build();
 }
