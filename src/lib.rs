ca66eda2140ba13abc2097d21b98a8928dbaf9dc
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-12-07 00:47:40 +0200

    Provides alternatives to missing functional macros
    NETMAP_TXRING/NETMAP_RXRING macros are not handled by bindgen so this
    commit provides altenative functions.

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,35 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+unsafe fn netmap_ring_offset(interface: &netmap_if, index: usize) -> ssize_t {
+    let offset_in_ring_ofs = std::mem::size_of_val(&*interface.ring_ofs.as_ptr()) * index;
+    let ring_offset_position = (*interface).ring_ofs.as_ptr().add(offset_in_ring_ofs);
+    *ring_offset_position
+}
+
+unsafe fn netmap_ring_by_offset(interface: &netmap_if, offset: ssize_t) -> *mut netmap_ring {
+    let base_addr = interface as *const _ as *const u8;
+    let netmap_ring_ptr = base_addr.offset(offset as isize);
+    netmap_ring_ptr as *mut netmap_ring
+}
+
+/// # Safety
+///
+/// [`index`] *must* be in [0, ni_tx_rings]
+pub unsafe fn netmap_txring(interface: *mut netmap_if, index: u16) -> *mut netmap_ring {
+    let interface = &*interface;
+    netmap_ring_by_offset(interface, netmap_ring_offset(interface, index as usize))
+}
+
+/// # Safety
+///
+/// [`index`] *must* be in [0, ni_rx_rings]
+pub unsafe fn netmap_rxring(interface: *mut netmap_if, index: u16) -> *mut netmap_ring {
+    let interface = &*interface;
+    let index = index as u32 + interface.ni_tx_rings + interface.ni_host_tx_rings;
+    netmap_ring_by_offset(interface, netmap_ring_offset(interface, index as usize))
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
